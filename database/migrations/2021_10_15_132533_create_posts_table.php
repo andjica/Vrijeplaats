1c38f30f679d7b950294cfc4c54f1a454c119b05
andjica <andjaaa95@gmail.com>
2021-10-28 13:22:44 +0200

    change migration

--- database/migrations/2021_10_15_132533_create_posts_table.php
+++ database/migrations/2021_10_15_132533_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->text('main_description');
             $table->text('description');
             $table->string('phone')->nullable();
             $table->integer('city_id');
