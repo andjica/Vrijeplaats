6a1242f5081481417113f6023e1cd6975abf59b5
andjica <andjaaa95@gmail.com>
2021-11-04 16:11:54 +0100

    blade components for admin dashboard

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,245 @@
+<header id="header" class="main-header header-sticky header-sticky-smart header-style-10 text-uppercase bg-white">
+<div class="sticky-area-wrap"><div class="header-wrapper sticky-area border-bottom" style="">
+<div class="container-fluid">
+<nav class="navbar navbar-expand-xl">
+<div class="header-mobile d-flex d-xl-none flex-fill justify-content-between align-items-center">
+<div class="navbar-toggler toggle-icon" data-toggle="collapse" data-target="#navbar-main-menu">
+<span></span>
+</div>
+<a class="navbar-brand navbar-brand-mobile" href="index.html">
+<img src="images/logo.png" alt="TheDir">
+</a>
+<a class="mobile-button-search" href="#search-popup" data-gtf-mfp="true" data-mfp-options="{&quot;type&quot;:&quot;inline&quot;,&quot;mainClass&quot;:&quot;mfp-move-from-top mfp-align-top search-popup-bg&quot;,&quot;closeOnBgClick&quot;:false,&quot;showCloseBtn&quot;:false}"><i class="far fa-search"></i></a>
+</div>
+<div class="collapse navbar-collapse" id="navbar-main-menu">
+<a class="navbar-brand d-none d-xl-block" href="index.html">
+<img src="images/logo.png" alt="TheDir">
+</a>
+<div class="form-search form-search-style-04 d-flex mr-auto">
+<form>
+<div class="d-flex align-items-center">
+<div class="form-search-items d-flex">
+<div class="form-search-item d-flex align-items-center what border-right">
+<label for="key-word">What</label>
+<div class="input-group dropdown show bg-transparent">
+<input type="text" autocomplete="off" id="key-word" name="key-word" class="form-control bg-transparent border-0" placeholder="Ex: food, service, barber, hotel" data-toggle="dropdown" aria-haspopup="true">
+<a href="#" class="input-group-append text-decoration-none" data-toggle="dropdown">
+ <i class="fal fa-chevron-down"></i>
+</a>
+<ul class="dropdown-menu form-search-ajax" aria-labelledby="key-word">
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+<svg class="icon icon-pizza">
+<use xlink:href="#icon-pizza"></use>
+</svg>
+<span class="font-size-md">Foods &amp; Restaurants</span>
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+<svg class="icon icon-bed">
+<use xlink:href="#icon-bed"></use>
+</svg>
+<span class="font-size-md">Hotels &amp; Resorts</span>
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+<svg class="icon icon-pharmaceutical">
+<use xlink:href="#icon-pharmaceutical"></use>
+</svg>
+<span class="font-size-md">Healths &amp; Medicals</span>
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+<svg class="icon icon-cog">
+<use xlink:href="#icon-cog"></use>
+</svg>
+<span class="font-size-md">Services</span>
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+<svg class="icon icon-car">
+<use xlink:href="#icon-car"></use>
+</svg>
+<span class="font-size-md">Automotive</span>
+</a>
+</li>
+</ul>
+</div>
+</div>
+<div class="form-search-item d-flex align-items-center where">
+ <label for="region">Where</label>
+<div class="input-group dropdown show bg-transparent">
+<input type="text" autocomplete="off" id="region" name="region" class="form-control bg-transparent border-0" placeholder="San Francisco" data-toggle="dropdown" aria-haspopup="true">
+<a href="#" class="input-group-append text-decoration-none" data-toggle="dropdown">
+<i class="fal fa-chevron-down"></i>
+</a>
+<ul class="dropdown-menu form-search-ajax" aria-labelledby="region">
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Austin
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Boston
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Chicago
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Denver
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Los Angeles
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+New York
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+San Francisco
+</a>
+</li>
+<li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Seattle
+</a>
+</li>
+ <li class="dropdown-item item">
+<a href="#" class="link-hover-dark-white">
+Washington
+</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<button type="submit" class="btn btn-primary"><i class="fal fa-search"></i>
+</button>
+</div>
+</form>
+</div>
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="#">Explore<span class="caret"><i class="fas fa-angle-down"></i></span></a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item">
+<a class="nav-link" href="#"> layout
+<span class="caret"><i class="fas fa-angle-down"></i></span>
+</a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item"><a class="nav-link" href="explore-full-map-grid.html"> full
+map grid</a></li>
+<li class="nav-item"><a class="nav-link" href="explore-full-map-list.html"> full
+map list</a></li>
+<li class="nav-item"><a class="nav-link" href="explore-half-map-grid.html"> half
+map grid</a></li>
+<li class="nav-item"><a class="nav-link" href="explore-half-map-list.html"> half
+map list</a></li>
+<li class="nav-item"><a class="nav-link" href="explore-sidebar-grid.html"> sidebar
+grid</a></li>
+<li class="nav-item"><a class="nav-link" href="explore-sidebar-list.html"> sidebar
+list</a></li>
+</ul>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Listing details
+<span class="caret"><i class="fas fa-angle-down"></i></span>
+</a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item"><a class="nav-link" href="listing-details-full-gallery.html">
+full gallery</a></li>
+ <li class="nav-item"><a class="nav-link" href="listing-details-full-image.html">
+full image</a></li>
+<li class="nav-item"><a class="nav-link" href="listing-details-full-map.html">
+full map</a></li>
+<li class="nav-item"><a class="nav-link" href="listing-details-gallery.html">gallery</a>
+</li>
+<li class="nav-item"><a class="nav-link" href="listing-details-image.html"> image</a>
+</li>
+<li class="nav-item"><a class="nav-link" href="listing-details-no-image.html"> no
+image</a></li>
+</ul>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Blog <span class="caret"><i class="fas fa-angle-down"></i></span></a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item">
+<a class="nav-link" href="#">Blog layout<span class="caret"><i class="fas fa-angle-down"></i></span></a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item"><a class="nav-link" href="blog-listing-grid.html">
+grid</a></li>
+<li class="nav-item"><a class="nav-link" href="blog-listing-large-image.html">
+large image</a></li>
+<li class="nav-item"><a class="nav-link" href="blog-listing-with-sidebar.html">
+with sidebar</a></li>
+</ul>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Post
+<span class="caret"><i class="fas fa-angle-down"></i></span>
+</a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item"><a class="nav-link" href="blog-single-audio.html">
+audio</a></li>
+<li class="nav-item"><a class="nav-link" href="blog-single-gallery.html">
+gallery</a></li>
+<li class="nav-item"><a class="nav-link" href="blog-single-image.html">
+image</a></li>
+<li class="nav-item"><a class="nav-link" href="blog-single-video.html">video</a>
+</li>
+</ul>
+</li>
+</ul>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Docs <span class="caret"><i class="fas fa-angle-down"></i></span></a>
+<ul class="sub-menu x-animated x-fadeInUp">
+<li class="nav-item">
+<a class="nav-link" href="document/introduction.html">Documentation</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="starter/introduction.html">Get started
+</a>
+</li>
+</ul>
+</li>
+</ul>
+<div class="header-customize justify-content-end align-items-center d-none d-xl-flex">
+<div class="header-customize-item button-search">
+<a class="mobile-button-search" href="#search-popup" data-gtf-mfp="true" data-mfp-options="{&quot;type&quot;:&quot;inline&quot;,&quot;mainClass&quot;:&quot;mfp-move-from-top mfp-align-top search-popup-bg&quot;,&quot;closeOnBgClick&quot;:false,&quot;showCloseBtn&quot;:false}"><i class="far fa-search"></i></a>
+</div>
+@if(auth()->user())
+<div class="header-customize-item">
+<a href="{{asset('/logout')}}" class="link">
+<svg class="icon icon-user-circle-o">
+<use xlink:href="#icon-user-circle-o"></use>
+</svg>
+Logout</a>
+</div>
+@endif
+<div class="header-customize-item">
+<a href="page-submit-listing.html" class="btn btn-primary btn-icon-right">Add
+Listing <i class="far fa-angle-right"></i></a>
+</div>
+</div>
+</div>
+</nav>
+</div>
+</div></div>
+</header>
---
+++ resources/views/layouts/template-admin.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Vrijeplaats | </title>
+
+<script src="/cdn-cgi/apps/head/2oc_RD5SS6wgN5SiQnSEnWVNHg8.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"data-auto-a11y="true"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('/css/')}}/fontawesome.css">
+<link rel="stylesheet" href="{{asset('/css/')}}/magnific-popup.css">
+<link rel="stylesheet" href="{{asset('/css/')}}/slick.css">
+<link rel="stylesheet" href="{{asset('/css/')}}/animate.css">
+
+<link rel="stylesheet" href="{{asset('/css/')}}/style.css">
+
+
+</head>
+<body>
+<div id="site-wrapper" class="site-wrapper panel dashboards">
+
+@include('admin.header')
+
+@yield('content')
+
+
+@include('components.footer')
+
+</div>
+
+
+</body>
+</html>
