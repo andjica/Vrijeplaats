5548e07884ba0af965a3e71b3dca320659a9908b
andjica <andjaaa95@gmail.com>
2021-12-15 16:10:58 +0100

    city model edited

--- app/City.php
+++ app/City.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Post;
 class City extends Model
 {
-    //
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'city_id');
+    }
 }
